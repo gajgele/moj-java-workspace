12e5c12349e715b95bf871aeaa811fe2575df27d
Darko Gajić <Darko Gaji>
2016-12-13 11:19:07 +0100

    dodavanje workspace-a

---
+++ Gaja03 - Nizovi/src/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] Ime={"Ime1", "Ime2", "Ime3"};
+		String[] Prezime={"Prezime1", "Prezime2", "Prezime3"};
+		
+		
+		for (int i=0;i<=2;i++){
+			System.out.println(Ime[i]+" "+Prezime[i]);
+		}
+		
+
+	}
+
+}
---
+++ Gaja04 - Petlje/src/App.java
@@ -0,0 +1,30 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 0;
+		
+		while(value <= 100){
+			System.out.println("Iteracija broj: " + value);
+			value++;
+			
+		}
+		int value2 = 30;
+		
+		while(value2 <= 50){
+			System.out.println("Iteracija broj: " + value2);
+			value2++;
+			
+		}
+		int value3 = 50;
+		
+		while(value3 >= 30){
+			System.out.println("Iteracija broj: " + value3);
+			value3--;
+			
+		}
+
+	}
+
+}
